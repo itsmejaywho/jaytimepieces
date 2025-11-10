b5151c7d0ebaf8bc4a833df5aa5c53086669dfda
Richmon Jay Francisco <jayjo.frncsc@gmail.com>
2025-11-10 15:23:03 +0800

    latest commit

--- frontend/script.js
+++ frontend/script.js
@@ -1,10 +1,23 @@
 const  logReg = document.getElementById('login-registration')
 const discoverMore = document.getElementById('discoverMore');
-
-
+const rating = document.getElementById('rating')
 const brands = document.getElementById('brands');
+const brandWatch = ['Seiko', 'Rolex', 'Tudor', 'Seiko', 'Rolex', 'Tudor','Seiko', 'Rolex', 'Tudor', 'Seiko', 'Rolex', 'Tudor', ];
+const howManyRate = document.getElementById('howManyRate')
+const stars = 5;
+const peopleRate = [2, 5, 5, 4, 3, 2, 5];
+let total = 0;
+let totalRating = 0
 
-const brandWatch = ['Seiko', 'Rolex', 'Tudor', 'Seiko', 'Rolex', 'Tudor','Seiko', 'Rolex', 'Tudor', 'Seiko', 'Rolex', 'Tudor',];
+
+peopleRate.forEach(rate => {
+    total = total + rate
+    totalRating = Number((total/peopleRate.length).toFixed(1))
+})
+
+
+rating.innerText =  `${totalRating} out of ${stars}`;
+howManyRate.innerText = `from ${peopleRate.length} reviews Worldwide`;
 
 
 brandWatch.forEach(brandName => {
