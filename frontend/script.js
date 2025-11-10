c58c53274183ee03e7452d5b501ea9a6d739de5b
Richmon Jay Francisco <jayjo.frncsc@gmail.com>
2025-11-11 02:23:42 +0800

    complete info

--- frontend/script.js
+++ frontend/script.js
@@ -4,18 +4,38 @@ const rating = document.getElementById('rating')
 const brands = document.getElementById('brands');
 const brandWatch = ['Seiko', 'Rolex', 'Tudor', 'Seiko', 'Rolex', 'Tudor','Seiko', 'Rolex', 'Tudor', 'Seiko', 'Rolex', 'Tudor', ];
 const howManyRate = document.getElementById('howManyRate')
+const userCount = document.getElementById('userCount')
+const protectionCount = document.getElementById('protectionCount');
+const buyerCount = document.getElementById('buyerCount')
+const buyerProtection = 100;
+const buyerCounts = 1000;
 const stars = 5;
 const peopleRate = [2, 5, 5, 4, 3, 2, 5];
 let total = 0;
 let totalRating = 0
+const users = peopleRate.length
 
+
+console.log(100000/1000)
 
 peopleRate.forEach(rate => {
     total = total + rate
     totalRating = Number((total/peopleRate.length).toFixed(1))
 })
 
+if(peopleRate.length > 0){
 
+    if(users < 999){
+        userCount.innerText = users
+    }else if(users >=1000 || users <= 999999){
+        userCount.innerText = Math.floor(peopleRate/1000) + 'thousand'
+    }else if (users >= 1000000 || users <= 100000000){
+        userCount.innerText = Math.floor(peopleRate/1000000) + 'million'
+    }
+}
+
+protectionCount.innerText = 'Over' + buyerProtection
+buyerCount.innerText = 'More than ' + buyerCounts
 rating.innerText =  `${totalRating} out of ${stars}`;
 howManyRate.innerText = `from ${peopleRate.length} reviews Worldwide`;
 
