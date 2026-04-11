ae7329b70ffe7f59f4303f60d17c111d00ef36ac
itsmejaywho <jayjo.frncsc@gmail.com>
2026-04-12 02:36:40 +0800

    CI: add ESLint Github Actions workflow

--- eslint.config.js
+++ eslint.config.js
@@ -7,7 +7,7 @@ import { defineConfig, globalIgnores } from 'eslint/config'
 export default defineConfig([
   globalIgnores(['dist']),
   {
-    files: ['**/*.{js,jsx}'],
+    files: ['*/.{js,jsx}'],
     extends: [
       js.configs.recommended,
       reactHooks.configs.flat.recommended,
@@ -23,7 +23,35 @@ export default defineConfig([
       },
     },
     rules: {
+      // Variables
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      'no-var': 'error',
+      'prefer-const': 'error',
+
+      // Best practices
+      'eqeqeq': ['error', 'always'],
+      'no-console': 'warn',
+      'no-debugger': 'error',
+      'no-alert': 'error',
+      'no-duplicate-imports': 'error',
+      'no-else-return': 'error',
+      // 'no-nested-ternary': 'error',                 # This is very strict. It means you can't do condition ? (other ? a : b) : c
+      'no-unneeded-ternary': 'error',
+      'prefer-template': 'error',
+
+      // Spacing & formatting
+      'semi': ['error', 'never'],
+      'quotes': ['error', 'single', { avoidEscape: true }],
+      'indent': ['error', 2, { SwitchCase: 1 }],
+      // 'comma-dangle': ['error', 'always-multiline'],       # Feel cluttered
+      'no-multiple-empty-lines': ['error', { max: 1 }],
+      'no-trailing-spaces': 'error',
+      'object-curly-spacing': ['error', 'always'],
+      'array-bracket-spacing': ['error', 'never'],
+      'arrow-spacing': 'error',
+      'key-spacing': 'error',
+      'space-before-blocks': 'error',
+      'keyword-spacing': 'error',
     },
   },
 ])
